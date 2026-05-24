61534c17c4e71df9c7fd891a6ebfd02951284b23
Ahmed Abdelbaset <140833881+ahmedabdelbaset902@users.noreply.github.com>
2026-05-25 01:45:13 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -175,5 +175,5 @@ const heroVideo = document.querySelector(".hero-video");
 const cardVideo = document.querySelector(".card-video");
 
 // تبطي السرعة
-heroVideo.playbackRate = 0.4;
-cardVideo.playbackRate = 0.4;
+heroVideo.playbackRate = 0.10;
+cardVideo.playbackRate = 0.10;
